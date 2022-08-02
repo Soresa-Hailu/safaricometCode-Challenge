e179ad1cf677c611aa213eeccffc88611c5b0e59
craftgithubuser <craftknowledge.soresa@gmail.com>
2022-08-02 09:56:34 -0400

    databasetable

--- Student/src/main/java/com/student/entity/StudentEntity.java
+++ Student/src/main/java/com/student/entity/StudentEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-@Entity(name = "users")
+@Entity(name = "students")
 public class StudentEntity implements Serializable {
 
 	private static final long serialVersionUID = -7702063606321069459L;
